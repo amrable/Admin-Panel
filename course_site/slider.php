905100c1d2c71c769eb29e564ee0a33408a50380
amrfahmy <amrfahmy57@gmail.com>
2019-09-26 12:51:54 +0200

    Show sliders on client panel

--- course_site/slider.php
+++ course_site/slider.php
@@ -1,41 +1,36 @@
+<?php
+	require '../site_admin/connect_database.php';
+	$sql = "select * from slider";
+	$sliders=$conn->query($sql);
+ ?>
+
 	<section id="home">
 		<div class="home-pattern"></div>
-		<div id="main-carousel" class="carousel slide" data-ride="carousel"> 
+		<div id="main-carousel" class="carousel slide" data-ride="carousel">
+
 			<ol class="carousel-indicators">
-				<li data-target="#main-carousel" data-slide-to="0" class="active"></li>
-				<li data-target="#main-carousel" data-slide-to="1"></li>
-				<li data-target="#main-carousel" data-slide-to="2"></li>
-			</ol><!--/.carousel-indicators--> 
+				<?php foreach ($sliders as $key => $value) { ?>
+					<li data-target="#main-carousel" data-slide-to="<?php echo $key ; ?>" class=" <?php if ( $key==0 ) echo ' active' ; ?> "></li>
+				<?php } ?>
+			</ol>
 			<div class="carousel-inner">
-				<div class="item active" style="background-image: url(images/slider/slide3.jpg)"> 
-					<div class="carousel-caption"> 
-						<div> 
-							<h2 class="heading animated bounceInDown">'Himu' Onepage HTML Template</h2> 
-							<p class="animated bounceInUp">Fully Professional one page template</p> 
-							<a class="btn btn-default slider-btn animated fadeIn" href="#">Get Started</a> 
-						</div> 
-					</div> 
-				</div>
-				<div class="item" style="background-image: url(images/slider/slide2.jpg)"> 
-					<div class="carousel-caption"> <div> 
-						<h2 class="heading animated bounceInDown">Get All in Onepage</h2> 
-						<p class="animated bounceInUp">Everything is outstanding </p> <a class="btn btn-default slider-btn animated fadeIn" href="#">Get Started</a> 
-					</div> 
-				</div> 
-			</div> 
-			<div class="item" style="background-image: url(images/slider/slide1.jpg)"> 
-				<div class="carousel-caption"> 
-					<div> 
-						<h2 class="heading animated bounceInRight">Fully Responsive Template</h2> 
-						<p class="animated bounceInLeft">100% Responsive HTML template</p> 
-						<a class="btn btn-default slider-btn animated bounceInUp" href="#">Get Started</a> 
-					</div> 
-				</div> 
-			</div>
-		</div><!--/.carousel-inner-->
+				<?php foreach ($sliders as $key => $value) { ?>
+
+					<div class="item <?php if ( $key==0 ) echo ' active' ; ?> " style="background-image: url(images/slider/<?php echo $value['img']; ?>)">
+						<div class="carousel-caption">
+							<div>
+								<h2 class="heading animated bounceInDown">  <?php echo $value['title'];  ?></h2>
+								<p class="animated bounceInUp"><?php echo $value['description']; ?> </p>
+								<a class="btn btn-default slider-btn animated fadeIn" href="#">Get Started</a>
+							</div>
+						</div>
+					</div>
+
+			<?php } ?>
+		</div>
 
 		<a class="carousel-left member-carousel-control hidden-xs" href="#main-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
 		<a class="carousel-right member-carousel-control hidden-xs" href="#main-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>
-	</div> 
+	</div>
 
 </section><!--/#home-->
